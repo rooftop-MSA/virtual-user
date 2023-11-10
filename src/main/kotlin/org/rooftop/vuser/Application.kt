91c45381a21700a8b5c706af01e4f350fd17b050
xb205 <62425964+devxb@users.noreply.github.com>
2023-11-10 17:26:46 +0900

    init: 프로젝트 초기 세팅 (#2)

---
+++ src/main/kotlin/org/rooftop/vuser/Application.kt
@@ -0,0 +1,12 @@
+package org.rooftop.vuser
+
+internal class Application {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            TODO("RUN SPRING APPLICATION")
+        }
+    }
+
+}
